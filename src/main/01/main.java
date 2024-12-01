f02139b396978b2b32237770e8d0ad06f12a7b2e
dasJake <dev.die.das.jake@gmail.com>
2024-12-01 20:52:24 +0100

    calculate absolute diff of 2 lists

---
+++ src/main/01/main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.*;
+import java.io.*;
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("advent day1");
+        
+        ClassLoader classloader = main.class.getClassLoader();
+        File inputFile = new File(classloader.getResource("resources/input/01/input.txt").getFile());
+
+        List<Integer> leftlist = new ArrayList<>();
+        List<Integer> rightlist = new ArrayList<>(); 
+//        int[] listOfDifferencesLeftToRight = new int[leftlist.size()];
+//        List<Integer> combinedList = Stream.concat(leftlist.stream(), rightlist.stream().map(x -> -x)).collect(Collectors.toList());
+                
+        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                String[] lineValues = line.trim().split("\\s+");
+
+                leftlist.add(Integer.parseInt(lineValues[0]));
+                rightlist.add(Integer.parseInt(lineValues[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Collections.sort(leftlist);
+        Collections.sort(rightlist);
+        List<Integer> diffList = IntStream.range(0, Math.min(leftlist.size(), rightlist.size()))
+            .mapToObj(i -> Math.abs(leftlist.get(i) - rightlist.get(i)))
+            .collect(Collectors.toList());
+        
+        int sumDiffList = diffList.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(sumDiffList);
+    }
+}
